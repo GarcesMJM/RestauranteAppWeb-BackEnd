b87491c85b93b597d2fd559a1f219c6301f6f8e8
GarcesMJM <89167308+GarcesMJM@users.noreply.github.com>
2023-11-16 21:14:22 -0500

    Se agrega funcionalidad al controlador obtener usuario

--- controllers/obtenerusuarioController.js
+++ controllers/obtenerusuarioController.js
@@ -2,7 +2,22 @@ async function obtenerUsuario(req , res){
     try {
         const {token} = req.body;
 
+        if (!token) {
+            return res.status(200).json({ message: 'No hay token' });
+          }
         
+          jwt.verify(token, 'your-secret-key', (err, decoded) => {
+            if (err) {
+              return res.status(200).json({ message: 'Invalid token' });
+            }
+    
+            usuario = decoded;
+            db.query('SELECT * FROM users WHERE id = ?', [usuario.id], async (error, results) => {
+
+            });
+        
+            next();
+          });
         
     } catch (error) {
         
