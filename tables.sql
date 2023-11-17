fa900288d75644b0f8e7376732d560752ef2e797
Sebastian Rueda <sebastian.ruedamarin@windriver.com>
2023-11-17 00:47:44 -0500

    alertas

--- tables.sql
+++ tables.sql
@@ -7,12 +7,20 @@ CREATE TABLE users (
   hash VARCHAR(1000)
 );
 
-CREATE TABLE bookings ( 
+CREATE TABLE bookings (
   id INT PRIMARY KEY AUTO_INCREMENT, 
   name VARCHAR(60),
   email VARCHAR(30),
   num INT,
-  sede VARCHAR(10),
+  sede VARCHAR(15) FOREIGN KEY REFERENCES sedes(name),
+  date DATE,
+  time TIME
+);
+
+CREATE TABLE sedes ( 
+  name VARCHAR(15) PRIMARY KEY,
+  cupos_totales INT,
+  cupos_disp INT,
   date DATE,
   time TIME
 );
